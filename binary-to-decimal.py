c1939a67dd85638d57ddc8176434e1b3ca7164c6
Gavin-McQuaid <GavinMcQuaid123@gmail.com>
2015-11-09 19:42:08 +0000

    Create binary-to-decimal.py
    Main file

---
+++ binary-to-decimal.py
@@ -0,0 +1,11 @@
+import sys
+
+s = str(sys.argv[1]) # The binary number is taken as a command line argument
+decimal_number = 0
+i = 0
+while i < len(s):
+	if s[len(s)-1-i] == '1':
+		total += 2 ** i
+	i += 1
+
+print decimal_number  #print decimal form of the binary number
